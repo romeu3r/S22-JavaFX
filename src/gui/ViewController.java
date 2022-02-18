bc55be1aae0970ccfb859c0f9f9a323f2b2da985
romeu3r <romeu3r@gmail.com>
2022-02-18 00:08:13 -0300

    Test implements sum method.

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -4,14 +4,31 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
     @FXML
-    public Button btTest;
+    public TextField txtNumber1;
+    @FXML
+    public TextField txtNumber2;
 
     @FXML
-    public void onBtTestAction() {
-        Alerts.showAlert("Alert Title", null, "Hello!", Alert.AlertType.CONFIRMATION);
+    public Label labelResult;
+
+    @FXML
+    public Button btSum;
+
+    @FXML
+    public void onBtSumAction() {
+        try {
+            double num1 = Double.parseDouble(txtNumber1.getText());
+            double num2 = Double.parseDouble(txtNumber2.getText());
+            double sum = num1 + num2;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
